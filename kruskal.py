c121089d415175510177db1e64a856bf2461d6f5
Ramona <ramona.orsi2@studenti.iulm.it>
2025-05-11 15:22:57 +0200

    Update kruskal.py

--- kruskal.py
+++ kruskal.py
@@ -1,5 +1,3 @@
-Python 3.13.0 (v3.13.0:60403a5409f, Oct  7 2024, 00:37:40) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
 import streamlit as st
 import numpy as np
 import pandas as pd
